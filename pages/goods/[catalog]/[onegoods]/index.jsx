2a5378f3c6d50d596f5d7a67376155f95a820d7f
rostikowb <rostikowb132@gmail.com>
2021-02-06 19:55:57 +0200

    sitemap onegoods fix

--- pages/goods/[catalog]/[onegoods]/index.jsx
+++ pages/goods/[catalog]/[onegoods]/index.jsx
@@ -19,78 +19,85 @@ export const getStaticPaths = async () => {
 
   const goodsId = await fetchAllGoodsId()
   let links = [];
-
-  console.log('goodsId: ', goodsId.length);
-
-  // const translit = new cyrillicToTranslit()
-  // const name = translit.transform(goodsId[0]["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
-  // console.log(goodsId[0].ctgrId);
-  // const paths = [
-  //   {params: {catalog: goodsId[0].ctgrId.toString(), onegoods: `${goodsId[0]._id}__${name}`}, locale: 'ua'},
-  //   {params: {catalog: goodsId[0].ctgrId.toString(), onegoods: `${goodsId[0]._id}__${name}`}, locale: 'ru'}
-  // ]
+  let paths = [];
+  let goodsLang = goods.length;
 
 
-  // const paths = goodsId.map((post) => {
-  //   if (!post["nm"]) return console.log('error id: ' + post['_id']);
-  //
-  //   const translit = new cyrillicToTranslit()
-  //   const name_RU = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
-  //   const url_RU = `/goods/${post.ctgrId}/${post._id}__${name_RU}`;
-  //   const name_UA = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
-  //   const url_UA = `/goods/${post.ctgrId}/${post._id}__${name_UA}`;
-  //
-  //   links.push({
-  //     url_UA,
-  //     changefreq: 'weekly',
-  //     links: [
-  //       { lang: 'ua', url: url_UA },
-  //       { lang: 'ua-ru', url: url_RU }
-  //     ]
-  //   });
-  //
-  //
+
+
+  while (goodsLang--) {
+    const post = goodsId[goodsLang];
+    console.log('goodsId: ', goodsLang);
+
+    if (!post["nm"]) {
+      console.log('error id: ' + post['_id']);
+      continue;
+    }
+
+    const translit = new cyrillicToTranslit()
+    const name_RU = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
+    const url_RU = `/goods/${post.ctgrId}/${post._id}__${name_RU}`;
+    const name_UA = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
+    const url_UA = `/goods/${post.ctgrId}/${post._id}__${name_UA}`;
+    const UApath = {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ua'};
+    const RUpath = {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ru'};
+
+    const param = {
+      url_UA,
+      changefreq: 'weekly',
+      links: [
+        {lang: 'ua', url: url_UA},
+        {lang: 'ua-ru', url: url_RU}
+      ]
+    };
+
+    links.push(param)
+    paths = [...paths, UApath, RUpath]
+
+  }
+  console.log('linksParseFinish');
+
   // })
 
-  const pathsUA = goodsId.map((post) => {
-    const nm = post["nm_UA"] || post["nm"]
-    if (nm) {
-      const translit = new cyrillicToTranslit()
-      const name = translit.transform(nm.replace(/[^a-zа-яё\d]/ig, '_'));
-      const url = `/goods/${post.ctgrId}/${post._id}__${name}`;
+  // const pathsUA = goodsId.map((post) => {
+  //   const nm = post["nm_UA"] || post["nm"]
+  //   if (nm) {
+  //     const translit = new cyrillicToTranslit()
+  //     const name = translit.transform(nm.replace(/[^a-zа-яё\d]/ig, '_'));
+  //     const url = `/goods/${post.ctgrId}/${post._id}__${name}`;
+  //
+  //     const sitemap = {url, changefreq: 'weekly', links: []}
+  //     sitemap.links.push({ lang: 'ua', url })
+  //     links.push(sitemap);
+  //
+  //     return {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ua'}
+  //   } else {
+  //     console.log('error id: ' + post['_id']);
+  //   }
+  //
+  //   console.log('linksParseFinish');
+  //   })
+  //
+  // const pathsRU = goodsId.map((post) => {
+  //
+  //   if (post["nm"]) {
+  //     const translit = new cyrillicToTranslit()
+  //     const name = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
+  //     const url = `/ru/goods/${post.ctgrId}/${post._id}__${name}`;
+  //
+  //     const sitemap = {url, changefreq: 'weekly', links: []}
+  //     sitemap.links.push({ lang: 'ua-ru', url: url })
+  //     links.push(sitemap);
+  //
+  //     return {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ru'}
+  //   } else {
+  //     console.log('error id: ' + post['_id']);
+  //   }
 
-      const sitemap = {url, changefreq: 'weekly', links: []}
-      sitemap.links.push({ lang: 'ua', url })
-      links.push(sitemap);
 
-      return {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ua'}
-    } else {
-      console.log('error id: ' + post['_id']);
-    }
+  // })
 
-    console.log('linksParseFinish');
-    })
-
-  const pathsRU = goodsId.map((post) => {
-
-    if (post["nm"]) {
-      const translit = new cyrillicToTranslit()
-      const name = translit.transform(post["nm"].replace(/[^a-zа-яё\d]/ig, '_'));
-      const url = `/ru/goods/${post.ctgrId}/${post._id}__${name}`;
-
-      const sitemap = {url, changefreq: 'weekly', links: []}
-      sitemap.links.push({ lang: 'ua-ru', url: url })
-      links.push(sitemap);
-
-      return {params: {catalog: post.ctgrId.toString(), onegoods: `${post._id}__${name}`}, locale: 'ru'}
-    } else {
-      console.log('error id: ' + post['_id']);
-    }
-
-    console.log('linksParseFinish');
-  })
-
-  const paths = [...pathsUA, ...pathsRU]
+  // const paths = [...pathsUA, ...pathsRU]
 
   links = addMorePageToSitemap(links)
   const stream = new SitemapStream({hostname: option.STATIC})
